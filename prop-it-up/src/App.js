c956f9fa0a21e0ad01071294edf92ab3d49a1f9c
Ibraheem <ibraheem.shwiki@gmail.com>
2021-07-14 11:44:00 +0300

    propitup

---
+++ prop-it-up/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="persons">
+        <PersonCard firstName = "Ibraheem" lastName = "Shweiki" age = {32} hairColor = "Brown"/>
+      </div>
+      <div className="persons">
+        <PersonCard firstName = "Layth" lastName = "Irshaid" age = {23} hairColor = "Black"/>
+      </div>
+      <div className="persons">
+        <PersonCard firstName = "Rohronoa" lastName = "Zoro" age = {29} hairColor = "Green"/>
+      </div>
+      <div className="persons">
+        <PersonCard firstName = "Nami" lastName = "Bartillo" age = {23} hairColor = "Orange"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
