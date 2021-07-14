c9d76c3fa581c8b3c5dcf59b22936540252b7dbd
Ibraheem <ibraheem.shwiki@gmail.com>
2021-07-14 16:59:09 +0300

    putItAllTogether

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 export class PersonCard extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                age: this.props.age,
+            }
+        }
+        handleBirthday = () => this.setState({
+            age: this.state.age + 1
+        });
+    
     render() {
         return (
             <div>
                 <h1>{ this.props.firstName }, { this.props.lastName }</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.handleBirthday}>Happy Birthday {this.props.lastName}</button>
             </div>
         )
     }
 }
+
 
 export default PersonCard
