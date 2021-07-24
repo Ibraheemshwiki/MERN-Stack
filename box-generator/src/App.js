9b029ad76ae8df8b031f1ce81b9a16c8d09dc8c8
Ibraheem <ibraheem.shwiki@gmail.com>
2021-07-24 19:12:17 +0300

    box generator

---
+++ box-generator/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import BoxMaker from './components/BoxMaker';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets Make some Boxes</h1>
+      <p>pick a color and size you want the box to be</p>
+      <BoxMaker/>
+    </div>
+  );
+}
+
+export default App;
