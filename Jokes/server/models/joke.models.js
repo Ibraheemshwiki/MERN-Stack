00577716c065abe32dd3601106fa127874834dac
Ibraheem <ibraheem.shwiki@gmail.com>
2021-07-28 16:51:22 +0300

    few new assignments

---
+++ Jokes/server/models/joke.models.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const JokeSchema = new mongoose.Schema({
+    setup: { type: String },
+    punchLine: { type: String }
+   }, { timestamps: true })
+   const Joke = mongoose.model('joke', JokeSchema);
+
+   module.exports = Joke;
