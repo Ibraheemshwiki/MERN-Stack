7c09777b3c0ff48075e89467b67bed2ab8edce99
Ibraheem <ibraheem.shwiki@gmail.com>
2021-08-02 10:58:50 +0300

    the real boxy

--- boxy/src/components/Input.js
+++ boxy/src/components/Input.js
@@ -5,14 +5,27 @@ export const Input = () => {
     const [color, setColor] = useState("");
     const submitHandler = (e) => {
         e.preventDefault();
-        setListColors([...listColors,color]);
+        setListColors(listColors.concat({color}));
 }
     return (
+        <>
         <div>
             <form onSubmit = {submitHandler}>
-                <input onChange ={ (e) => setColor(e.target.value)}></input>
+                <input onChange ={ (e) => setColor(e.target.value)} value={color}></input>
                 <button>Add Whatever</button>
             </form>
         </div>
+        <div className = "boxes">
+       { listColors.map((box, i) => {
+            return(
+                <div key = {i} style = {{backgroundColor:box.color, width:200+"px", height:200+"px", margin:10+"px"}}></div>
+
+                    )
+    
+                }
+            )
+        }
+        </div>
+    </>
     )
 }
